27300284a55ce938838b23e2b352c517b6dab285
kshitijrajawat <101655005+kshitijrajawat@users.noreply.github.com>
2026-03-10 02:28:00 +0530

    Update api.js

--- hungrezy-client/src/utils/api.js
+++ hungrezy-client/src/utils/api.js
@@ -4,8 +4,21 @@ const API = axios.create({
   baseURL: "/api/v1",
 });
 
+export const DIRECT_API = axios.create({
+  baseURL: "https://api.kshitijrajawat.in/api/v1",
+});
+
 // Attach JWT token to every request automatically
 API.interceptors.request.use((config) => {
+  const auth = localStorage.getItem("auth");
+  if (auth) {
+    const { token } = JSON.parse(auth);
+    if (token) config.headers.Authorization = token;
+  }
+  return config;
+});
+
+DIRECT_API.interceptors.request.use((config) => {
   const auth = localStorage.getItem("auth");
   if (auth) {
     const { token } = JSON.parse(auth);
